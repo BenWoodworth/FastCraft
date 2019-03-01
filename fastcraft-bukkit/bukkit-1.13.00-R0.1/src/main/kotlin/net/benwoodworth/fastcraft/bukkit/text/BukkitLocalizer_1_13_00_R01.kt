3fb12f6600089889ba9b22eb31a0b95c8e6b4e27
BenWoodworth <ben@benwoodworth.net>
2019-03-01 16:55:36 -0500

    Implement BukkitLocalizer_1_13_00_R01.

--- fastcraft-bukkit/bukkit-1.13.00-R0.1/src/main/kotlin/net/benwoodworth/fastcraft/bukkit/text/BukkitLocalizer_1_13_00_R01.kt
+++ fastcraft-bukkit/bukkit-1.13.00-R0.1/src/main/kotlin/net/benwoodworth/fastcraft/bukkit/text/BukkitLocalizer_1_13_00_R01.kt
@@ -1,12 +1,31 @@
 package net.benwoodworth.fastcraft.bukkit.text
 
 import net.benwoodworth.fastcraft.platform.text.FcLocale
+import org.bukkit.Bukkit
 import javax.inject.Inject
 
 class BukkitLocalizer_1_13_00_R01 @Inject constructor(
 ) : BukkitLocalizer {
 
+    private val defaultLocale: Map<String, String>
+
+    init {
+        val entryRegex = Regex(""".*?"(.*)"\s*:\s*"(.*)".*""")
+
+        defaultLocale = Bukkit::class.java
+            .getResource("/assets/minecraft/lang/en_us.json")
+            ?.openStream()
+            ?.reader()
+            ?.useLines { lines ->
+                lines
+                    .mapNotNull { entryRegex.matchEntire(it)?.destructured }
+                    .map { (key, value) -> key to value }
+                    .toMap()
+            }
+            ?: emptyMap()
+    }
+
     override fun localize(key: String, locale: FcLocale): String? {
-        return key // TODO
+        return defaultLocale[key] // TODO Use locale
     }
 }
