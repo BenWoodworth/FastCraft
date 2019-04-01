59ec4d49da4bbae883c44f9ec79f341dee0b7dff
BenWoodworth <ben@benwoodworth.net>
2019-03-31 21:17:49 -0400

    Update profiling code.

--- fastcraft/src/main/kotlin/net/benwoodworth/fastcraft/Profiling.kt
+++ fastcraft/src/main/kotlin/net/benwoodworth/fastcraft/Profiling.kt
@@ -4,10 +4,10 @@ inline fun <T> profile(desc: String, block: () -> T): T {
     val begin = System.nanoTime()
     println("fcprofile\t$desc\tbegin\t$begin")
 
-    val result = block()
-
-    val end = System.nanoTime()
-    println("fcprofile\t$desc\tend\t$end\t+${end - begin}")
-
-    return result
+    try {
+        return block()
+    } finally {
+        val end = System.nanoTime()
+        println("fcprofile\t$desc\tend\t$end\t+${end - begin}")
+    }
 }
