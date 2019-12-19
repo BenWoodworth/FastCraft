7ad71d86b6270c7f22966462a1244d2634e66d08
BenWoodworth <ben@benwoodworth.net>
2019-12-19 00:29:00 -0500

    Implement IngredientProductProvider for Bukkit 1.15.

--- src/bukkit/kotlin/net/benwoodworth/fastcraft/bukkit/recipe/IngredientProductProvider_1_15_00_R01.kt
+++ src/bukkit/kotlin/net/benwoodworth/fastcraft/bukkit/recipe/IngredientProductProvider_1_15_00_R01.kt
@@ -1,11 +1,20 @@
 package net.benwoodworth.fastcraft.bukkit.recipe
 
+import org.bukkit.Material
 import org.bukkit.inventory.ItemStack
 import javax.inject.Inject
 
 class IngredientProductProvider_1_15_00_R01 @Inject constructor(
 ) : IngredientProductProvider {
     override fun getIngredientProducts(item: ItemStack): List<ItemStack> {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        return when (item.type) {
+            Material.LAVA_BUCKET,
+            Material.MILK_BUCKET,
+            Material.WATER_BUCKET -> {
+                List(item.amount) { ItemStack(Material.BUCKET) }
+            }
+
+            else -> emptyList()
+        }
     }
 }
