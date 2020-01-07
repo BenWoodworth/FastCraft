32fa636460a9850659cddc45007cb722979256f2
BenWoodworth <ben@benwoodworth.net>
2020-01-07 02:14:40 -0500

    Add ItemAmounts class.

---
+++ src/common/kotlin/net/benwoodworth/fastcraft/crafting/model/ItemAmounts.kt
@@ -0,0 +1,42 @@
+package net.benwoodworth.fastcraft.crafting.model
+
+import net.benwoodworth.fastcraft.platform.item.FcItem
+import net.benwoodworth.fastcraft.platform.item.FcItemFactory
+import java.util.*
+import javax.inject.Inject
+
+class ItemAmounts private constructor(
+    private val ingredients: MutableMap<FcItem, Int>,
+    private val itemFactory: FcItemFactory
+) {
+    private companion object {
+        val keys = WeakHashMap<FcItem, FcItem>()
+    }
+
+    @Inject
+    constructor(itemFactory: FcItemFactory) : this(mutableMapOf(), itemFactory)
+
+    fun FcItem.asKey(): FcItem {
+        return when (amount) {
+            1 -> this
+            else -> keys.getOrPut(this) {
+                itemFactory.copyItem(this, 1)
+            }
+        }
+    }
+
+    operator fun get(item: FcItem): Int {
+        return ingredients.getOrDefault(item, 0)
+    }
+
+    operator fun set(item: FcItem, amount: Int) {
+        when (amount) {
+            0 -> ingredients.remove(item.asKey())
+            else -> ingredients[item.asKey()] = amount
+        }
+    }
+
+    fun copy(): ItemAmounts {
+        return ItemAmounts(ingredients.toMutableMap(), itemFactory)
+    }
+}
