55663d48011132dde15bad41880450bcd5bd816f
BenWoodworth <ben@benwoodworth.net>
2020-01-14 21:24:16 -0500

    Add FcPlayer.giveItems() function.

--- src/bukkit/kotlin/net/benwoodworth/fastcraft/bukkit/player/BukkitFcPlayer_1_15_00_R01.kt
+++ src/bukkit/kotlin/net/benwoodworth/fastcraft/bukkit/player/BukkitFcPlayer_1_15_00_R01.kt
@@ -2,15 +2,18 @@ package net.benwoodworth.fastcraft.bukkit.player
 
 import com.google.auto.factory.AutoFactory
 import com.google.auto.factory.Provided
+import net.benwoodworth.fastcraft.bukkit.item.toItemStack
 import net.benwoodworth.fastcraft.bukkit.text.BukkitFcLocale_1_15_00_R01
 import net.benwoodworth.fastcraft.bukkit.text.BukkitFcText
 import net.benwoodworth.fastcraft.bukkit.text.BukkitFcTextConverter
+import net.benwoodworth.fastcraft.platform.item.FcItem
 import net.benwoodworth.fastcraft.platform.player.FcPlayer
 import net.benwoodworth.fastcraft.platform.player.FcPlayerInventory
 import net.benwoodworth.fastcraft.platform.text.FcLocale
 import net.benwoodworth.fastcraft.platform.text.FcText
 import org.bukkit.Server
 import org.bukkit.entity.Player
+import org.bukkit.inventory.ItemStack
 import java.util.*
 
 @AutoFactory
@@ -58,6 +61,21 @@ class BukkitFcPlayer_1_15_00_R01(
 
     override fun hasPermission(permission: String): Boolean {
         return player.hasPermission(permission)
+    }
+
+    override fun giveItems(items: List<FcItem>, dropAll: Boolean) {
+        fun ItemStack.drop() {
+            player.world.dropItemNaturally(player.location, this)
+        }
+
+        items.forEach { item ->
+            if (dropAll) {
+                item.toItemStack().drop()
+            } else {
+                val notAdded = player.inventory.addItem(item.toItemStack()).values
+                notAdded.forEach { it.drop() }
+            }
+        }
     }
 
     override fun equals(other: Any?): Boolean {
--- src/common/kotlin/net/benwoodworth/fastcraft/platform/player/FcPlayer.kt
+++ src/common/kotlin/net/benwoodworth/fastcraft/platform/player/FcPlayer.kt
@@ -1,5 +1,6 @@
 package net.benwoodworth.fastcraft.platform.player
 
+import net.benwoodworth.fastcraft.platform.item.FcItem
 import net.benwoodworth.fastcraft.platform.text.FcLocale
 import net.benwoodworth.fastcraft.platform.text.FcText
 import java.util.*
@@ -20,6 +21,8 @@ interface FcPlayer {
     fun sendMessage(message: FcText)
 
     fun hasPermission(permission: String): Boolean
+
+    fun giveItems(items: List<FcItem>, dropAll: Boolean)
 
     override fun equals(other: Any?): Boolean
 
