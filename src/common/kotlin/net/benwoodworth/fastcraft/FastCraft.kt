9f366535452ec683ba648eb99ee042d2cab5be05
BenWoodworth <ben@benwoodworth.net>
2020-01-16 01:35:26 -0500

    Change plugin start behavior.

--- src/common/kotlin/net/benwoodworth/fastcraft/FastCraft.kt
+++ src/common/kotlin/net/benwoodworth/fastcraft/FastCraft.kt
@@ -4,16 +4,12 @@ import net.benwoodworth.fastcraft.crafting.FastCraftGuiFactory
 import net.benwoodworth.fastcraft.platform.player.FcPlayerJoinEvent
 import net.benwoodworth.fastcraft.platform.server.FcServer
 import net.benwoodworth.fastcraft.platform.server.FcTaskFactory
-import net.benwoodworth.fastcraft.platform.text.FcTextColors
-import net.benwoodworth.fastcraft.platform.text.FcTextFactory
 import javax.inject.Inject
 
 class FastCraft @Inject internal constructor(
     serverListeners: FcServer,
     private val taskFactory: FcTaskFactory,
-    private val fastCraftGuiFactory: FastCraftGuiFactory,
-    private val textFactory: FcTextFactory,
-    private val textColors: FcTextColors
+    private val fastCraftGuiFactory: FastCraftGuiFactory
 ) {
     init {
         serverListeners.onPlayerJoin += ::onPlayerJoin
@@ -23,24 +19,7 @@ class FastCraft @Inject internal constructor(
     }
 
     private fun onPlayerJoin(event: FcPlayerJoinEvent) {
-        event.player.sendMessage(
-            textFactory.createFcText(
-                text = "Welcome to ",
-                color = textColors.aqua,
-                extra = listOf(
-                    textFactory.createFcText(
-                        text = "the ",
-                        color = textColors.green
-                    ),
-                    textFactory.createFcText(
-                        text = "server!",
-                        bold = true
-                    )
-                )
-            )
-        )
-
-        val task = taskFactory.createFcTask(delaySeconds = 5.0) {
+        val task = taskFactory.createFcTask(delaySeconds = 2.0) {
             fastCraftGuiFactory
                 .createFastCraftGui(event.player)
                 .open()
