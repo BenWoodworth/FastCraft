0551546319d189d1dff817562e2bbae638da8c26
BenWoodworth <ben@benwoodworth.net>
2020-01-20 23:18:07 -0500

    Disable opening FastCraft GUI on startup.

--- src/common/kotlin/net/benwoodworth/fastcraft/FastCraft.kt
+++ src/common/kotlin/net/benwoodworth/fastcraft/FastCraft.kt
@@ -2,28 +2,25 @@ package net.benwoodworth.fastcraft
 
 import net.benwoodworth.fastcraft.crafting.FastCraftGuiFactory
 import net.benwoodworth.fastcraft.platform.player.FcPlayerEvents
-import net.benwoodworth.fastcraft.platform.player.FcPlayerJoinEvent
-import net.benwoodworth.fastcraft.platform.server.FcTaskFactory
+import net.benwoodworth.fastcraft.platform.player.FcPlayerOpenWorkbenchEvent
 import javax.inject.Inject
 
 class FastCraft @Inject internal constructor(
     playerEventsListeners: FcPlayerEvents,
-    private val taskFactory: FcTaskFactory,
     private val fastCraftGuiFactory: FastCraftGuiFactory
 ) {
     init {
-        playerEventsListeners.onPlayerJoin += ::onPlayerJoin
+        playerEventsListeners.onPlayerOpenWorkbench += ::onPlayerOpenWorkbench
     }
 
     fun disable() {
     }
 
-    private fun onPlayerJoin(event: FcPlayerJoinEvent) {
-        val task = taskFactory.createFcTask(delaySeconds = 2.0) {
-            fastCraftGuiFactory
-                .createFastCraftGui(event.player)
-                .open()
-        }
-        task.schedule()
+    private fun onPlayerOpenWorkbench(event: FcPlayerOpenWorkbenchEvent) {
+        event.cancel()
+
+        fastCraftGuiFactory
+            .createFastCraftGui(event.player)
+            .open()
     }
 }
