027063d6c847651f5bf1ef77f261e66205e37744
Ben Woodworth <ben@benwoodworth.net>
2020-02-08 17:56:39 -0500

    Add Bukkit API version check.

--- src/bukkit/kotlin/net/benwoodworth/fastcraft/bukkit/BukkitFastCraftPlugin.kt
+++ src/bukkit/kotlin/net/benwoodworth/fastcraft/bukkit/BukkitFastCraftPlugin.kt
@@ -1,13 +1,18 @@
 package net.benwoodworth.fastcraft.bukkit
 
 import net.benwoodworth.fastcraft.FastCraft
+import net.benwoodworth.fastcraft.bukkit.util.BukkitVersion
+import org.bukkit.Bukkit
 import org.bukkit.plugin.java.JavaPlugin
+import java.util.logging.Level
 
 @Suppress("unused")
 class BukkitFastCraftPlugin : JavaPlugin() {
     private var fastCraft: FastCraft? = null
 
     override fun onEnable() {
+        checkBukkitVersion()
+
         val factory = DaggerBukkitFastCraftFactory_1_15_00_R01.builder()
             .bukkitDaggerModule_1_15_00_R01(BukkitDaggerModule_1_15_00_R01(this))
             .build()
@@ -23,5 +28,22 @@ class BukkitFastCraftPlugin : JavaPlugin() {
     override fun reloadConfig() {
         super.reloadConfig()
         // TODO
+    }
+
+    private fun checkBukkitVersion() {
+        val minSupported = BukkitVersion.parse("1.15")!!
+        val minUnsupported = BukkitVersion.parse("1.16")!!
+
+        val versionStr = Bukkit.getBukkitVersion()
+        val version = BukkitVersion.parse(versionStr)
+
+        when {
+            version == null || version >= minUnsupported -> {
+                logger.log(Level.WARNING, "Bukkit API $versionStr may not be supported.")
+            }
+            version < minSupported -> {
+                logger.log(Level.SEVERE, "Not compatible with Bukkit API $versionStr.")
+            }
+        }
     }
 }
---
+++ src/bukkit/kotlin/net/benwoodworth/fastcraft/bukkit/util/BukkitVersion.kt
@@ -0,0 +1,42 @@
+package net.benwoodworth.fastcraft.bukkit.util
+
+data class BukkitVersion(
+    val major: Int,
+    val minor: Int,
+    val patch: Int,
+    val rMajor: Int,
+    val rMinor: Int
+) : Comparable<BukkitVersion> {
+    override fun compareTo(other: BukkitVersion): Int {
+        return compareValuesBy(
+            this,
+            other,
+            { it.major },
+            { it.minor },
+            { it.patch },
+            { it.rMajor },
+            { it.rMinor }
+        )
+    }
+
+    override fun toString(): String {
+        return "$major.$minor.$patch-R$rMajor.$rMinor"
+    }
+
+    companion object {
+        private val versionRegex = Regex("""^(\d+)(?:\.(\d+)(?:\.(\d+))?)?(?:-R(\d+)(?:.(\d+))?)?(?:-.*)?$""")
+
+        fun parse(version: String): BukkitVersion? {
+            val match = versionRegex.matchEntire(version) ?: return null
+            val (major, minor, patch, rMajor, rMinor) = match.destructured
+
+            return BukkitVersion(
+                major = major.toIntOrNull() ?: 0,
+                minor = minor.toIntOrNull() ?: 0,
+                patch = patch.toIntOrNull() ?: 0,
+                rMajor = rMajor.toIntOrNull() ?: 0,
+                rMinor = rMinor.toIntOrNull() ?: 0
+            )
+        }
+    }
+}
