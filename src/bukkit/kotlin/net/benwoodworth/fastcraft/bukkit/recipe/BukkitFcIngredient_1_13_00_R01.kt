413f9920780e58877f6cfacabe7b96161ba75056
Ben Woodworth <ben@benwoodworth.net>
2020-02-25 13:12:03 -0500

    Add BukkitFcIngredient_1_13_00_R01.

---
+++ src/bukkit/kotlin/net/benwoodworth/fastcraft/bukkit/recipe/BukkitFcIngredient_1_13_00_R01.kt
@@ -0,0 +1,51 @@
+package net.benwoodworth.fastcraft.bukkit.recipe
+
+import net.benwoodworth.fastcraft.bukkit.item.itemStack
+import net.benwoodworth.fastcraft.platform.item.FcItem
+import org.bukkit.Material
+import org.bukkit.inventory.ItemStack
+import java.util.*
+
+
+class BukkitFcIngredient_1_13_00_R01(
+    val slotIndex: Int,
+    val ingredientItem: ItemStack
+) : BukkitFcIngredient {
+    init {
+        require(slotIndex in 0..8)
+    }
+
+    private companion object {
+        const val WILDCARD_DATA: Short = -1
+    }
+
+    @Suppress("DEPRECATION")
+    override fun matches(item: FcItem): Boolean {
+        val bukkitItem = item.itemStack
+
+        return when {
+            bukkitItem.type === Material.AIR -> {
+                ingredientItem.type == Material.AIR
+            }
+            ingredientItem.durability == WILDCARD_DATA -> {
+                ingredientItem.durability = bukkitItem.durability
+
+                ingredientItem.isSimilar(bukkitItem)
+                    .also { ingredientItem.durability = WILDCARD_DATA }
+            }
+            else -> {
+                ingredientItem.isSimilar(bukkitItem)
+            }
+        }
+    }
+
+    override fun equals(other: Any?): Boolean {
+        return other is BukkitFcIngredient_1_13_00_R01 &&
+                slotIndex == other.slotIndex &&
+                ingredientItem == other.ingredientItem
+    }
+
+    override fun hashCode(): Int {
+        return Objects.hash(slotIndex, ingredientItem)
+    }
+}
