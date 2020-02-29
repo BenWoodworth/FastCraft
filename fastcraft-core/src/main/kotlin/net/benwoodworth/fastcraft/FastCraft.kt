b1cae16620768f2f78484a76d2ebd0e2856e8057
Ben Woodworth <ben@benwoodworth.net>
2020-02-29 10:08:38 -0500

    Add message about commands/config not being implemented yet.

--- fastcraft-core/src/main/kotlin/net/benwoodworth/fastcraft/FastCraft.kt
+++ fastcraft-core/src/main/kotlin/net/benwoodworth/fastcraft/FastCraft.kt
@@ -3,15 +3,19 @@ package net.benwoodworth.fastcraft
 import net.benwoodworth.fastcraft.crafting.FastCraftGuiFactory
 import net.benwoodworth.fastcraft.platform.player.FcPlayerEvents
 import net.benwoodworth.fastcraft.platform.player.FcPlayerOpenWorkbenchEvent
+import net.benwoodworth.fastcraft.platform.server.FcLogger
 import javax.inject.Inject
 
 class FastCraft @Inject internal constructor(
     playerEventsListeners: FcPlayerEvents,
-    private val fastCraftGuiFactory: FastCraftGuiFactory
+    private val fastCraftGuiFactory: FastCraftGuiFactory,
+    private val logger: FcLogger
 ) {
     init {
         Strings.load()
         playerEventsListeners.onPlayerOpenWorkbench += ::onPlayerOpenWorkbench
+
+        logger.info("Note: Commands and configuration have not been implemented yet, and will be added in a future release.")
     }
 
     fun disable() {
