1c9b1558e3218ea5b712d5377bb096344bcbc740
ben <ben@benwoodworth.net>
2020-05-13 15:49:46 -0400

    Remove unused parameter from BukkitFcCommandRegistry_1_7

--- fastcraft-bukkit/bukkit-1.7/src/main/kotlin/net/benwoodworth/fastcraft/bukkit/command/BukkitFcCommandRegistry_1_7.kt
+++ fastcraft-bukkit/bukkit-1.7/src/main/kotlin/net/benwoodworth/fastcraft/bukkit/command/BukkitFcCommandRegistry_1_7.kt
@@ -2,7 +2,6 @@ package net.benwoodworth.fastcraft.bukkit.command
 
 import net.benwoodworth.fastcraft.platform.command.FcCommand
 import net.benwoodworth.fastcraft.platform.server.FcLogger
-import net.benwoodworth.fastcraft.platform.text.FcTextConverter
 import org.bukkit.Server
 import org.bukkit.plugin.Plugin
 import javax.inject.Inject
@@ -14,7 +13,6 @@ class BukkitFcCommandRegistry_1_7 @Inject constructor(
     private val server: Server,
     private val logger: FcLogger,
     private val commandAdapterFactory: BukkitFcCommandAdapter.Factory,
-    private val textConverter: FcTextConverter,
 ) : BukkitFcCommandRegistry {
     override fun register(command: FcCommand, name: String, vararg aliases: String) {
         val pluginCommand = server.getPluginCommand("${plugin.name}:$name")
