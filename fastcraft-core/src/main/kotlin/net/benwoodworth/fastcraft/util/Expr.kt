cf43f4af664185d66edd27508d2381d05fb46e63
ben <ben@benwoodworth.net>
2020-05-13 20:53:34 -0400

    Allow Expr expressions to be treated as regular expressions

--- fastcraft-core/src/main/kotlin/net/benwoodworth/fastcraft/util/Expr.kt
+++ fastcraft-core/src/main/kotlin/net/benwoodworth/fastcraft/util/Expr.kt
@@ -2,6 +2,13 @@ package net.benwoodworth.fastcraft.util
 
 data class Expr(val expression: String) {
     fun toRegex(): Regex {
+        if (expression.length >= 2 &&
+            expression.startsWith('/') &&
+            expression.endsWith('/')
+        ) {
+            return Regex(expression.substring(1 until expression.lastIndex))
+        }
+
         return Regex.escape(expression)
             .replace("*", """\E.*\Q""")
             .let { Regex("^$it$") }
