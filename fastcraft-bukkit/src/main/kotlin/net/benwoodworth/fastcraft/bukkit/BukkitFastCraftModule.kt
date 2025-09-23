0ce0d1481e4b49146bf705470ac7b9547c8a7cd8
Ben Woodworth <ben@benwoodworth.com>
2025-09-23 17:20:42 -0400

    Implement CraftingInventoryViewFactory for Bukkit 1.15

--- fastcraft-bukkit/src/main/kotlin/net/benwoodworth/fastcraft/bukkit/BukkitFastCraftModule.kt
+++ fastcraft-bukkit/src/main/kotlin/net/benwoodworth/fastcraft/bukkit/BukkitFastCraftModule.kt
@@ -209,11 +209,13 @@ class BukkitFastCraftModule(
 
     @Provides
     fun provideCraftingInventoryViewFactory(
+        instance_1_15: Provider<CraftingInventoryViewFactory_1_15>,
         instance_1_14: Provider<CraftingInventoryViewFactory_1_14>,
         instance_1_9: Provider<CraftingInventoryViewFactory_1_9>,
         instance_1_7: Provider<CraftingInventoryViewFactory_1_7>,
     ): CraftingInventoryViewFactory {
         return when {
+            bukkitVersion >= VERSION_1_15 -> instance_1_15.get()
             bukkitVersion >= VERSION_1_14 -> instance_1_14.get()
             bukkitVersion >= VERSION_1_9 -> instance_1_9.get()
             bukkitVersion >= VERSION_1_7 -> instance_1_7.get()
